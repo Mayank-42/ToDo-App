f8b08270e4287837b58341cbbc5f277423fd9064
Mayank Raj <mayank.rj2004@gmail.com>
2026-09-16 12:59:15 -0700

    created the function database builder

--- app/src/main/java/com/example/t0d0app/Di/dataModule.kt
+++ app/src/main/java/com/example/t0d0app/Di/dataModule.kt
@@ -1,4 +1,46 @@
 package com.example.t0d0app.Di
 
-class dataModule {
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import com.example.t0d0app.data.local.todoDatabase
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): todoDatabase {
+
+        return Room.databaseBuilder(
+            context,
+            todoDatabase::class.java,
+            "todo_database"
+        )
+            .addCallback(object : RoomDatabase.Callback() {
+
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+
+                    db.execSQL(
+                        "INSERT INTO Task (task, isMarked) VALUES ('SWIPE TO DELETE', 0)"
+                    )
+
+                    db.execSQL(
+                        "INSERT INTO Task (task, isMarked) VALUES ('Click on add button', 0)"
+                    )
+                }
+            })
+            .build()
+    }
 }
